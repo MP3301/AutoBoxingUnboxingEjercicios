ed6037fda0012290aab574375591d890273a0965
x <x@mail.com>
2022-07-31 05:28:10 -0400

    //el boxing es convertir una variable primitiva a un wrapper         // el unboxing es extraer una variable wrapper a un tipo primitivo

---
+++ domain/TestAutoboxingUnboxing.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package domain;
+
+/**
+ *
+ * @author asosadjpasdijasixapoapxsasxas
+ */
+public class TestAutoboxingUnboxing {
+    
+    public static void main(String[] args) {
+        
+        Integer entero = 10;
+        
+        int enteroInt;
+        
+        //el boxing es convertir una variable primitiva a un wrapper
+        // el unboxing es extraer una variable wrapper a un tipo primitivo
+        enteroInt = entero;
+        
+        System.out.println("enteroInt="+enteroInt);
+        
+    }
+    
+    
+    
+}
